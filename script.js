23b3ce80d9a4f7953842d9d4f8acf84bb6d07820
Geoff Nix <FullStackGeoff@gmail.com>
2020-10-16 01:12:47 -0400

    drilled down to fields needed in console.log

--- script.js
+++ script.js
@@ -18,11 +18,33 @@ $(document).ready(function () {
 
 	$.ajax(zCall).done(function (response) {
 		console.log(response);
-		console.log(response.link);
 		for (let i = 0; i < response.nearby_restaurants.length; i++) {
-			console.log(response.nearby_restaurants[i]);
-			console.log(response.nearby_restaurants[i].restaurant.name);
-			console.log(response.nearby_restaurants[i].restaurant.url);
+			// console.log(response.nearby_restaurants[i]);
+			console.log(
+				"nearby restaurants: " +
+					response.nearby_restaurants[i].restaurant.name
+			);
+			console.log(
+				"Address: " +
+					response.nearby_restaurants[i].restaurant.location.address
+			);
+
+			console.log(
+				"Average cost for two: " +
+					response.nearby_restaurants[i].restaurant
+						.average_cost_for_two
+			);
+			// console.log("Menu: " + response.restaurant);
+			console.log(
+				"Specialty Dish: " +
+					response.nearby_restaurants[i].restaurant.cuisines
+			);
+
+			console.log(
+				"url: " + response.nearby_restaurants[i].restaurant.url
+			);
+			console.log("_____________________________________________");
+			console.log("_____________________________________________");
 		}
 	});
 });
