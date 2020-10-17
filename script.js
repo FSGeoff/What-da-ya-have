92c8e19ca676a22e6af2d3175226546c81c4baf9
Geoff Nix <FullStackGeoff@gmail.com>
2020-10-17 13:41:57 -0400

    added on click to buttons

--- script.js
+++ script.js
@@ -2,51 +2,46 @@ $(document).ready(function () {
 	var latitude = "33.954600";
 	var longitude = "-84.501590";
 
-	const zCall = {
-		async: true,
-		crossDomain: true,
-		url:
-			"https://developers.zomato.com/api/v2.1/geocode?lat=" +
-			latitude +
-			"&lon=" +
-			longitude,
-		method: "GET",
-		headers: {
-			"user-key": "5c3b3a505188c7c86aa5ca5bf3a93cf9",
-		},
-	};
+	function restaurantCall(lon, lat) {
+		const zCall = {
+			async: true,
+			crossDomain: true,
+			url:
+				"https://developers.zomato.com/api/v2.1/geocode?lat=" +
+				lat +
+				"&lon=" +
+				lon,
+			method: "GET",
+			headers: {
+				"user-key": "5c3b3a505188c7c86aa5ca5bf3a93cf9",
+			},
+		};
+		$.ajax(zCall).done(function (response) {
+			console.log(response);
+			for (let i = 0; i < response.nearby_restaurants.length; i++) {
+				var restArray = response.nearby_restaurants;
+				console.log(
+					"nearby restaurants: " + restArray[i].restaurant.name
+				);
+				console.log(
+					"Address: " + restArray[i].restaurant.location.address
+				);
 
-	$.ajax(zCall).done(function (response) {
-		console.log(response);
-		for (let i = 0; i < response.nearby_restaurants.length; i++) {
-			// console.log(response.nearby_restaurants[i]);
-			console.log(
-				"nearby restaurants: " +
-					response.nearby_restaurants[i].restaurant.name
-			);
-			console.log(
-				"Address: " +
-					response.nearby_restaurants[i].restaurant.location.address
-			);
+				console.log(
+					"Average cost for two: " +
+						restArray[i].restaurant.average_cost_for_two
+				);
+				// console.log("Menu: " + response.restaurant);
+				console.log(
+					"Specialty Dish: " + restArray[i].restaurant.cuisines
+				);
 
-			console.log(
-				"Average cost for two: " +
-					response.nearby_restaurants[i].restaurant
-						.average_cost_for_two
-			);
-			// console.log("Menu: " + response.restaurant);
-			console.log(
-				"Specialty Dish: " +
-					response.nearby_restaurants[i].restaurant.cuisines
-			);
-
-			console.log(
-				"url: " + response.nearby_restaurants[i].restaurant.url
-			);
-			console.log("_____________________________________________");
-			console.log("_____________________________________________");
-		}
-	});
+				console.log("url: " + restArray[i].restaurant.url);
+				console.log("_____________________________________________");
+				console.log("_____________________________________________");
+			}
+		});
+	}
 
 	trainStops = [
 		{ station: "Decatur", longitude: "-84.29485", latitude: "33.774784" },
@@ -88,4 +83,13 @@ $(document).ready(function () {
 
 		{ station: "Lenox", longitude: "-84.356098", latitude: "33.846843" },
 	];
+
+	$(".js-station").on("click", function () {
+		console.log($(this).text().trim());
+		for (let i = 0; i < trainStops.length; i++) {
+			if (trainStops[i].station === $(this).text().trim()) {
+				restaurantCall(trainStops[i].longitude, trainStops[i].latitude);
+			}
+		}
+	});
 });
