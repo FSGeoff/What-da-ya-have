ef0eea2f39793fac6c0eed80029d18b8d3161f39
Adebambo Adeshiyan <badeshiyan@gmail.com>
2020-10-20 19:06:57 -0400

    updated weather.js - worked on weather and marti apis

--- weather.js
+++ weather.js
@@ -15,23 +15,23 @@ $(document).ready(function () {
   // "http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apikey=1b88df3e-8bf3-472c-ae67-2497f94f1d0f";
 
   // set marta api through ajax function
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-    var objectCount = Object.keys(response).length;
-    console.log("object count: " + objectCount);
-    // var martaArray = new Array[objectCount]();
+  // $.ajax({
+  //   url: queryURL,
+  //   method: "GET",
+  // }).then(function (response) {
+  //   console.log(response);
+  //   var objectCount = Object.keys(response).length;
+  //   console.log("object count: " + objectCount);
+  //   // var martaArray = new Array[objectCount]();
 
-    // create an array of objects of each Marta station and the match long/lat numbers (gold and green lines)
-    martaArray = response;
-    console.log(martaArray);
-    console.log(martaArray[0].DESTINATION);
-    console.log(martaArray[0].STATION);
-    console.log(martaArray[0].LINE);
-    console.log(martaArray[0].WAITING_SECONDS);
-  });
+  //   // create an array of objects of each Marta station and the match long/lat numbers (gold and green lines)
+  //   martaArray = response;
+  //   console.log(martaArray);
+  //   console.log(martaArray[0].DESTINATION);
+  //   console.log(martaArray[0].STATION);
+  //   console.log(martaArray[0].LINE);
+  //   console.log(martaArray[0].WAITING_SECONDS);
+  // });
 
   //  revisit later to see if needed
   //   for (let i = 0; i < response.response.docs.length; i++) {
